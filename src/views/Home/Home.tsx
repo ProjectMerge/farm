09305c5463914a3bf197c16d7a8c0b141e167af1
dfwplay <dfwplay@gmail.com>
2021-08-13 10:43:47 -0500

    initial commit

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../../assets/img/logo.png'
+import Button from '../../components/Button'
+import Container from '../../components/Container'
+import Page from '../../components/Page'
+import PageHeader from '../../components/PageHeader'
+import Spacer from '../../components/Spacer'
+import Balances from './components/Balances'
+
+const Home: React.FC = () => {
+  return (
+    <Page>
+      <PageHeader
+        icon={<img src={logo} height={120} />}
+        title="Merge Farming"
+        subtitle="Stake MERGE/BNB LP tokens and claim your MERGE from the Bridge Pool"
+      />
+
+      <Container>
+        <Balances />
+      </Container>
+      <Spacer size="lg" />
+      <StyledInfo>
+        🏆<b>Pro Tip</b>: MERGE-BNB LP token pool yields more token per block.
+      </StyledInfo>
+      <Spacer size="lg" />
+      <div
+        style={{
+          margin: '0 auto',
+        }}
+      >
+        <Button text="🔪 Start Here" to="/farms" variant="secondary" />
+      </div>
+    </Page>
+  )
+}
+
+const StyledInfo = styled.h3`
+  color: ${(props) => props.theme.color.grey[500]};
+  font-size: 16px;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+
+  > b {
+    color: ${(props) => props.theme.color.grey[600]};
+  }
+`
+
+export default Home
